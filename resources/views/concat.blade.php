625d64f1d81a674c0e74a210dbcee57e3bc8cad6
hosein <85564616+hoseinalizadeh1373@users.noreply.github.com>
2023-03-30 12:06:32 +0430

    k

--- resources/views/concat.blade.php
+++ resources/views/concat.blade.php
@@ -4,15 +4,72 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+       <script src="https://vjs.zencdn.net/5.15/video.js"></script>
+<link href="https://vjs.zencdn.net/5.15/video-js.css" rel="stylesheet" />
 </head>
 <body>
-    <script>
+    {{-- <script>
            if (!crossOriginIsolated) SharedArrayBuffer = ArrayBuffer;
-    </script>
-    <video id="output-video" controls></video><br/>
+    </script> --}}
+    {{-- <video id="output-video" controls></video><br/>
     <input type="file" id="uploader">
     <p id="message"></p>
     <script src="https://unpkg.com/@ffmpeg/ffmpeg@0.8.1/dist/ffmpeg.min.js"></script>
-    <script src="/js/33.js"></script>
+    <script src="/js/33.js"></script> --}}
+
+ 
+
+
+<audio id="my-player" class="video-js" controls>
+    <source id='my-spanish-video-track' src="1.mp4" type="video/mp4" />
+  <source id="my-spanish-audio-track" src="https://www.w3schools.com/html/horse.ogg" type="audio/ogg">
+    
+</audio>
+<video ><source id='my-spanish-video-track' src="1.mp4" type="video/mp4" /></video>
+<script>
+    var player = videojs('my-player');
+    var track = new videojs.AudioTrack({
+  id: 'my-spanish-audio-track',
+  kind: 'translation',
+  label: 'Spanish',
+  language: 'es'
+});
+var tr = new videojs.VideoTrack({
+    id: 'my-spanish-video-track',
+  kind: 'translation',
+  label: 'Spanish',
+  language: 'es'
+});
+
+
+// Add the track to the player's audio track list.
+player.audioTracks().addTrack(track);
+player.videoTracks().addTrack(tr);
+// player.videoTracks().addTrack()
+</script> 
+
+{{-- <video
+id="my-video"
+class="video-js"
+controls
+preload="auto"
+width="640"
+height="264"
+poster="MY_VIDEO_POSTER.jpg"
+data-setup="{}"
+>
+<source src="/asset/Ringtone-3.mp3" type="audio/ogg" />
+<source src="1.mp4" type="video/mp4" />
+
+<p class="vjs-no-js">
+  To view this video please enable JavaScript, and consider upgrading to a
+  web browser that
+  <a href="https://videojs.com/html5-video-support/" target="_blank"
+    >supports HTML5 video</a
+  >
+</p>
+</video> --}}
+
+<script src="https://vjs.zencdn.net/8.0.4/video.min.js"></script>
 </body>
 </html>
--- resources/views/convert.blade.php
+++ resources/views/convert.blade.php
@@ -2,22 +2,39 @@
 <body>
     <video id="player" controls></video>
     <input type="file" id="uploader">
-    <script src="ffmpeg.min.js">
-      
+   <script>
+           if (!crossOriginIsolated) SharedArrayBuffer = ArrayBuffer;
     </script>
+    <script src="https://unpkg.com/@ffmpeg/ffmpeg@0.8.1/dist/ffmpeg.min.js"></script>
     <script>
       const { createFFmpeg, fetchFile } = FFmpeg;
       const ffmpeg = createFFmpeg({ log: true });
-      const transcode = async ({ target: { files } }) => {
-        const { name } = files[0];
-        await ffmpeg.load();
-        ffmpeg.FS('writeFile', name, await fetchFile(files[0]));
-        await ffmpeg.run('-i', name,  'output.mp4');
-        const data = ffmpeg.FS('readFile', 'output.mp4');
-        const video = document.getElementById('player');
-        video.src = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
-      }
+      const sourceBuffer = await fetch("/1.mp4").then(r => r.arrayBuffer());
+
+// create the FFmpeg instance and load it
+const ffmpeg = createFFmpeg({ log: true });
+// await ffmpeg.load();
+
+// write the AVI to the FFmpeg file system
+ffmpeg.FS(
+  "writeFile",
+  "/1.mp4",
+  new Uint8Array(sourceBuffer, 0, sourceBuffer.byteLength)
+);
+
+// run the FFmpeg command-line tool, converting the AVI into an MP4
+await ffmpeg.run("-i", "/1.mp4", "/outputdsds.mp4");
+
+// read the MP4 file back from the FFmpeg file system
+const output = ffmpeg.FS("readFile", "outputdsds.mp4");
+
+// ... and now do something with the file
+const video = document.getElementById("video");
+video.src = URL.createObjectURL(
+  new Blob([output.buffer], { type: "video/mp4" })
+);
       document
         .getElementById('uploader').addEventListener('change', transcode);
     </script>
+    
   </body>
